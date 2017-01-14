e21839d213afc7dfe37d72bc0ee77f94aab82a01
alfonso zbook <zbook@reyesaguayo.com>
2017-01-13 20:46:10 -0600

    moved to /opt

--- eda-matlab-martinez/matlab/software_01.m
+++
@@ -1,8 +0,0 @@
-load software
-% First transform the data.
-X = log(prepsloc);
-Y = log(defsloc);
-% Plot the transformed data.
-plot(X,Y,'.')
-xlabel('Log PrepTime/SLOC')
-ylabel('Log Defects/SLOC')
--- eda-matlab-martinez/matlab/software_02.m
+++
@@ -1,9 +0,0 @@
-% First generate some 2-D multivariate normal
-% random variables, with mean MU and 
-% covariance SIGMA. This uses a Statistics
-% Toolbox function.
-n = 100;
-mu = [-2, 2];
-sigma = [1,.5;.5,1];
-X = mvnrnd(mu,sigma,n);
-plot(X(:,1),X(:,2),'.')
